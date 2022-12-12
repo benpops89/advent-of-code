4d19b7cc66b452292ce04b433775b9fc67ac67fd
Ben Poppy <ben.poppy78@gmail.com>
2022-12-12 22:54:48 +0000

    chore: add solution to part 1

--- 2022/6/answer.py
+++ 2022/6/answer.py
@@ -1,9 +1,26 @@
 def parse():
-    with open("2022/6/input_test.txt", "r") as f:
-        data = list(map(str.strip("\n"), f.readlines()))
+    with open("2022/6/input.txt", "r") as f:
+        data = list(map(lambda x: x.strip("\n"), f.readlines()))
 
     return data
 
 
 if __name__ == "__main__":
-    data = parse()
+    data = parse()[0]
+
+    seen = []
+    start = 0
+    while len(seen) < 4:
+        for x in data[start:]:
+            if len(seen) == 4:
+                break
+
+            if x not in seen:
+                seen.append(x)
+            else:
+                del seen[:]
+                start += 1
+                break
+
+    answer1 = start + 4
+    print(answer1)
