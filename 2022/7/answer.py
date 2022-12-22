0a934f0c56df95879b1a2dccc915e0bdaf2828e6
Ben Poppy <ben.poppy78@gmail.com>
2022-12-22 09:33:24 +0000

    chore: add solutions for part 1 and 2

--- 2022/7/answer.py
+++ 2022/7/answer.py
@@ -1,8 +1,8 @@
-from itertools import chain
+from collections import defaultdict
 
 
 def parse():
-    with open("2022/7/input_test.txt", "r") as f:
+    with open("2022/7/input.txt", "r") as f:
         data = list(map(lambda x: x.strip("\n"), f.readlines()))
 
     return data
@@ -11,27 +11,40 @@ def parse():
 if __name__ == "__main__":
     data = parse()
 
-    fs = {}
-    dirs = set(["/"])
-    pwd = ["/"]
+    fs = defaultdict(int)
+    parents = []
+    pwd = []
     for line in data:
-        values = line.split(" ")
-        if values[0] == "$":
-            if values[1] == "cd":
-                if values[2] == "/":
-                    del pwd[1:]
-                elif values[2] == "..":
+        match line.split(" "):
+            case "$", "cd", directory:
+                if directory == "..":
                     pwd.pop()
+                    parents.pop()
+                elif directory == "/":
+                    pwd.clear()
+                    parents.clear()
                 else:
-                    pwd.append(values[2])
-        elif values[0] == "dir":
-            dirs.add(values[1])
-        elif values[0].isdigit():
-            for d in pwd:
-                fs.setdefault(d, []).append(int(values[0]))
+                    parents.append(f"/{'/'.join(pwd)}")
+                    pwd.append(directory)
+            case "$", "ls":
+                pass
+            case "dir", directory:
+                pass
+            case size, filename:
+                dirname = f"/{'/'.join(pwd)}"
+                fs[dirname] += int(size)
+                for parent in parents:
+                    print(parent)
+                    fs[parent] += int(size)
 
-    total_size = 100000
-    fs_reduced = {k: v for k, v in fs.items() if sum(v) <= total_size}
-
-    answer1 = sum(chain(*fs_reduced.values()))
+    answer1 = sum(filter(lambda x: x <= 100000, fs.values()))
     print(answer1)
+
+    total_size = 70000000
+    required_space = 30000000
+    needed_space = abs(total_size - fs["/"] - required_space)
+
+    answer2 = min(
+        filter(lambda x: x[1] >= needed_space, fs.items()), key=lambda x: x[1]
+    )
+    print(answer2)
