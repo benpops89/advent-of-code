8287cfdc662a553c1af241720c9e08bc84b0c54b
Ben Poppy <me@benpoppy.dev>
2024-12-08 23:41:55 +0000

    add solution for day 2 2024

--- 2024/2/solution.py
+++ 2024/2/solution.py
@@ -1,16 +1,28 @@
-# Part 1 - ?
-# Part 2 - ?
+# Part 1 - 242
+# Part 2 - 311
 
 
 class Solution:
-    PART1: int = 0
-    PART2: int = 0
+    PART1: int = 2
+    PART2: int = 4
 
     def __init__(self, data):
-        self.data = data
+        self.data = list(map(str.split, data))
 
-    def part1(self):
-        pass
+    @staticmethod
+    def is_safe(row) -> bool:
+        differences = [int(b) - int(a) for a, b in zip(row, row[1:])]
+        first_diff = differences[0]
+        return all(0 < abs(d) <= 3 and (d > 0) == (first_diff > 0) for d in differences)
 
-    def part2(self):
-        pass
+    def part1(self) -> int:
+        return sum(self.is_safe(row) for row in self.data)
+
+    def part2(self) -> int:
+        count = 0
+        for row in self.data:
+            permutations = [row[:i] + row[i + 1 :] for i in range(len(row))]
+            if any(self.is_safe(p) for p in permutations):
+                count += 1
+
+        return count
