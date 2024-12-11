e97e1455558f02ea94f2f0b0260d36eadecb799a
Ben Poppy <ben.poppy78@gmail.com>
2024-12-11 00:16:23 +0000

    add solution for day 4 2024 part2

--- 2024/4/solution.py
+++ 2024/4/solution.py
@@ -1,10 +1,10 @@
 # Part 1 - 2644
-# Part 2 - ?
+# Part 2 - 1952
 
 
 class Solution:
     PART1: int = 18
-    PART2: int = 0
+    PART2: int = 9
 
     def __init__(self, data):
         self.data = {
@@ -45,4 +45,22 @@ class Solution:
         return count
 
     def part2(self):
-        pass
+        count = 0
+        for point, letter in self.data.items():
+            if letter != "A":
+                continue
+
+            letters = []
+            for i, j in [(-1, -1), (1, 1), (-1, 1), (1, -1)]:
+                w = self.data.get((i + point[0], j + point[1]), None)
+                if w in ["M", "S"]:
+                    letters.append(w)
+
+            if len(letters) == 4:
+                left = letters[:2]
+                right = letters[2:]
+
+                if left[0] != left[1] and right[0] != right[1]:
+                    count += 1
+
+        return count
